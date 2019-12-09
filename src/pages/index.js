cf418c0a1edb2ba770d4da92ae7a5652f26755b0
Randy Burgess <wrburgess@gmail.com>
2019-12-08 21:48:29 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi folksters</h1>
+    <p>Welcome to your new Gatsby site. Yalp!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
